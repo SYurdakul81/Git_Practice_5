746f163f71103a9e14e271c26125dcdab6e2f021
Sezgin Yurdakul <58957641+sezginsy@users.noreply.github.com>
2022-04-23 12:48:53 +0200

    Commit-1: Added Hello_World class and a for loop to print out

--- src/Hello/Hello_World.java
+++ src/Hello/Hello_World.java
@@ -1,7 +1,13 @@
 package Hello;
-
+import java.util.Scanner;
 public class Hello_World {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter a number for number of printing Hello World");
+        int num=input.nextInt();
+        for (int i = 0; i <num ; i++) {
+            System.out.println("Hello World "+(i+1));
+        }
     }
 }
